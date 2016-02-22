0892add8a567d8e7fda15c4e680d5b0d430641f4
Johannes Asplund-Samuelsson <johannes.asplund.samuelsson@scilifelab.se>
2016-02-22 15:36:05 +0100

    Determination of reachable compound and reactant nodes and calculation of distance from origin

--- minet-path.py
+++ minet-path.py
@@ -12,18 +12,39 @@ from itertools import repeat
 
 # Define functions
 def CountRxns(path):
-    """Count the number of unique reactions in a path or list of nodes."""
+    """Count the number of unique reactions in a path graph."""
     rxns = set()
-    for node in path:
-        if node[0] != 'c':
-            rxns.add(node[1])
+    for node in path.nodes(data=True):
+        if node[1]['type'] != 'c':
+            rxns.add(node[1]['mid'])
     return len(rxns)
 
 def test_CountRxns():
-    p1 = [('c','C1'), ('rf','R1'), ('pf','R1'), ('c','C2')]
-    p2 = [('c','C3'), ('rr','R2'), ('pr','R2'), ('c','C4')]
-    p3 = [('c','C5')]
+    p1 = nx.DiGraph()
+    p1.add_path(range(1,5))
+    for node_data in enumerate([('c','C1'), ('rf','R1'), ('pf','R1'), ('c','C2')]):
+        n = node_data[0] + 1
+        p1.node[n]['type'] = node_data[1][0]
+        p1.node[n]['mid'] = node_data[1][1]
+
+    p2 = nx.DiGraph()
+    p2.add_path(range(1,5))
+    for node_data in enumerate([('c','C3'), ('rr','R2'), ('pr','R2'), ('c','C4')]):
+        n = node_data[0] + 1
+        p2.node[n]['type'] = node_data[1][0]
+        p2.node[n]['mid'] = node_data[1][1]
+
+    p3 = nx.DiGraph()
+    p3.add_node(1, type='c', mid='C5')
+
     p4 = [('c','C6'), ('rf','R3'), ('pf','R3'), ('c','C7'), ('rr','R3'), ('rp','R3'), ('c','C8')]
+    p4 = nx.DiGraph()
+    p4.add_path(range(1,8))
+    for node_data in enumerate([('c','C6'), ('rf','R3'), ('pf','R3'), ('c','C7'), ('rr','R3'), ('rp','R3'), ('c','C8')]):
+        n = node_data[0] + 1
+        p4.node[n]['type'] = node_data[1][0]
+        p4.node[n]['mid'] = node_data[1][1]
+
     assert CountRxns(p1) == 1
     assert CountRxns(p2) == 1
     assert CountRxns(p3) == 0
@@ -32,8 +53,7 @@ def test_CountRxns():
 
 def TrimNetwork(network):
     """Trims the fat off a network in order to improve multiprocessing memory usage."""
-    for node in network.nodes():
-        del network.node[node]['data']
+    del network.graph['mine_data']
 
 def test_TrimNetwork():
     # Set up testing networks
@@ -46,8 +66,11 @@ def test_TrimNetwork():
         G.add_path(p)
         Y.add_path(p)
     for n in G.nodes():
-        G.node[n]['data'] = ['dummy']
-        Y.node[n]['data'] = ['dummy']
+        G.node[n]['mid'] = 'dummy'
+        Y.node[n]['mid'] = 'dummy'
+
+    G.graph['mine_data'] = {'R1':{'_id':'R1'}}
+    Y.graph['mine_data'] = {'R1':{'_id':'R1'}}
 
     # Trim G but not Y
     TrimNetwork(G)
@@ -57,13 +80,277 @@ def test_TrimNetwork():
 
     # Data should be gone from G
     try:
-        x = [G.node[n]['data'] for n in G.nodes()]
+        x = G.graph['mine_data']
         d = True
     except KeyError:
         d = False
 
     assert d == False
 
+
+def FindStartCompNodes(network):
+    """Returns a list starting compound nodes in a MINE network."""
+    start_comp_nodes = []
+    for node in network.nodes():
+        if network.node[node]['type'] == 'c' and network.node[node]['start']:
+            start_comp_nodes.append(node)
+    return set(start_comp_nodes)
+
+def test_FindStartCompNodes():
+    G = nx.DiGraph()
+    G.add_node(1,type='c',start=True)
+    G.add_node(2,type='rf')
+    G.add_node(3,type='pf')
+    G.add_node(4,type='c',start=False)
+    G.add_path([1,2,3,4])
+    assert FindStartCompNodes(G) == set([1])
+
+
+def FindValidReactantNodes(network, comp_node_set=set([])):
+    """Find and return all reactant nodes that are valid given the supplied compound node set."""
+    # If the set is empty, use starting compounds as the compound node set
+    if comp_node_set == set([]):
+        comp_node_set = FindStartCompNodes(network)
+
+    valid_reactant_nodes = set([])
+
+    for node in network.nodes():
+        if network.node[node]['type'] in {'rf','rr'}:
+            c = network.node[node]['c']
+            if c.issubset(comp_node_set):
+                valid_reactant_nodes.add(node)
+
+    return valid_reactant_nodes
+
+def test_FindValidReactantNodes():
+    G = nx.DiGraph()
+    G.add_node(1,type='c',start=True)
+    G.add_node(2,type='rf',c={1})
+    G.add_node(3,type='pf',c={4})
+    G.add_node(4,type='c',start=False)
+    G.add_path([1,2,3,4])
+    G.add_node(9,type='rr',c={4})
+    G.add_node(10,type='pr',c={1})
+    G.add_path([4,9,10,1])
+    G.add_node(5,type='c',start=False)
+    G.add_node(6,type='rf',c={1,5})
+    G.add_node(7,type='pf',c={8})
+    G.add_node(8,type='c',start=False)
+    G.add_path([1,6,7,8])
+    G.add_edge(5,6)
+    G.add_node(11,type='rr',c={8})
+    G.add_node(12,type='pr',c={1,5})
+    G.add_path([8,11,12,1])
+    G.add_edge(12,5)
+    start_comps = FindStartCompNodes(G)
+    assert FindValidReactantNodes(G, start_comps) == set([2])
+    assert FindValidReactantNodes(G) == set([2])
+    assert FindValidReactantNodes(G, set([1,5])) == set([2,6])
+    assert FindValidReactantNodes(G, set([8])) == set([11])
+
+
+def ExpandValidCompoundSet(network, valid_reactant_nodes=set([]), comp_node_set=set([])):
+    """Expands the valid compound set with products of valid reactant nodes."""
+    if comp_node_set == set([]):
+        comp_node_set = FindStartCompNodes(network)
+    else:
+        for r_node in valid_reactant_nodes:
+            p_node = network.successors(r_node)[0]
+            comp_node_set = comp_node_set.union(network.node[p_node]['c'])
+    return comp_node_set
+
+def test_ExpandValidCompoundSet():
+    G = nx.DiGraph()
+    G.add_node(1,type='c',start=True)
+    G.add_node(2,type='rf',c={1})
+    G.add_node(3,type='pf',c={4})
+    G.add_node(4,type='c',start=False)
+    G.add_path([1,2,3,4])
+    G.add_node(9,type='rr',c={4})
+    G.add_node(10,type='pr',c={1})
+    G.add_path([4,9,10,1])
+    G.add_node(5,type='c',start=False)
+    G.add_node(6,type='rf',c={1,5})
+    G.add_node(7,type='pf',c={8})
+    G.add_node(8,type='c',start=False)
+    G.add_path([1,6,7,8])
+    G.add_edge(5,6)
+    G.add_node(11,type='rr',c={8})
+    G.add_node(12,type='pr',c={1,5})
+    G.add_path([8,11,12,1])
+    G.add_edge(12,5)
+    assert ExpandValidCompoundSet(G) == set([1])
+    assert ExpandValidCompoundSet(G, set([2,9]), set([1,4])) == set([1,4])
+    assert ExpandValidCompoundSet(G, FindValidReactantNodes(G, set([8])), set([8])) == set([1,5,8])
+    assert ExpandValidCompoundSet(G, set([2,6,11]), ExpandValidCompoundSet(G, set([11]), set([8]))) == set([1,4,5,8])
+
+
+def DistanceToOrigin(network, N=-1):
+    """
+    Calculates the shortest distance (number of reactions) from the starting compounds (origin) to
+    every node up to distance N. Set N to -1 to exhaustively calculate the minimum distance to
+    every node that is reachable.
+
+    Returns two sets in a tuple: Valid compound nodes and valid reactant nodes.
+    """
+
+    sys.stdout.write("\nCalculating minimum distance of nodes to origin...\n")
+    sys.stdout.flush()
+
+    # Set up counters
+    n = 0
+    c = 0
+    rf = 0
+    pf = 0
+    rr = 0
+    pr = 0
+
+    # Start with no valid reactant or compound nodes
+    valid_reactant_nodes = set([])
+    valid_compound_nodes = set([])
+
+    # The "previous" lists are also empty
+    prev_vrn = list(valid_reactant_nodes)
+    prev_vcn = list(valid_compound_nodes)
+
+    while True:
+
+        # Valid product nodes will be connected at the start of an expansion cycle
+        # They are however in principle identified in the previous cycle via valid reactant nodes
+        try:
+            for r_node in new_vrn:
+                p_node = network.successors(r_node)[0]
+                network.node[p_node]['dist'] = n
+                node_type = network.node[p_node]['type']
+                if node_type == 'pf':
+                    pf += 1
+                if node_type == 'pr':
+                    pr += 1
+        except NameError:
+            # new_vrn is not present in the first round
+            pass
+
+        # Expand the valid compound set
+        # When n = 0, this means the starting compounds
+        # When n > 0, the valid compound set will be expanded
+        new_vcn = ExpandValidCompoundSet(network, valid_reactant_nodes, valid_compound_nodes) - valid_compound_nodes
+        valid_compound_nodes = new_vcn.union(valid_compound_nodes)
+
+        new_vrn = FindValidReactantNodes(network, valid_compound_nodes) - valid_reactant_nodes
+        valid_reactant_nodes = new_vrn.union(valid_reactant_nodes)
+
+        for node in new_vcn:
+            network.node[node]['dist'] = n
+            c += 1
+        for node in new_vrn:
+            network.node[node]['dist'] = n
+            node_type = network.node[node]['type']
+            if node_type == 'rf':
+                rf += 1
+            if node_type == 'rr':
+                rr += 1
+
+        sys.stdout.write("Step %s:    %s c   %s rf   %s pf   %s rr   %s pr\n" % (str(n), str(c), str(rf), str(pf), str(rr), str(pr)))
+        sys.stdout.flush()
+
+        n += 1
+
+        if N == -1:
+            if set(prev_vrn) == valid_reactant_nodes and set(prev_vcn) == valid_compound_nodes:
+                # When no new valid compound or reactant nodes have been identified, it is time to stop
+                break
+        else:
+            if n > N:
+                # n starts at 0 and increments by one before each round dealing
+                # with that particular step n - stop when n exceeds the limit
+                break
+        prev_vrn = list(valid_reactant_nodes)
+        prev_vcn = list(valid_compound_nodes)
+
+    sys.stdout.write("\nDone.\n")
+    sys.stdout.flush()
+
+    return (valid_compound_nodes, valid_reactant_nodes)
+
+def test_DistanceToOrigin():
+    G = nx.DiGraph()
+    G.add_node(1,type='c',start=False)
+    G.add_node(2,type='rf',c={1})
+    G.add_node(3,type='pf',c={4})
+    G.add_node(4,type='c',start=False)
+    G.add_path([1,2,3,4])
+    G.add_node(9,type='rr',c={4})
+    G.add_node(10,type='pr',c={1})
+    G.add_path([4,9,10,1])
+    G.add_node(5,type='c',start=False)
+    G.add_node(6,type='rf',c={1,5})
+    G.add_node(7,type='pf',c={8})
+    G.add_node(8,type='c',start=True) # Compound 8 is now the start
+    G.add_path([1,6,7,8])
+    G.add_edge(5,6)
+    G.add_node(11,type='rr',c={8})
+    G.add_node(12,type='pr',c={1,5})
+    G.add_path([8,11,12,1])
+    G.add_edge(12,5)
+
+    output_0 = DistanceToOrigin(G.copy(), 0) # Creating a copy, since the function modifies the network it is given
+    output_1 = DistanceToOrigin(G.copy(), 1)
+    output_2 = DistanceToOrigin(G, 2) # Not creating a copy in order to check modification capabilities
+
+    assert output_0 == (set([8]), set([11])) # Compound and reactant nodes reachable within 0 reaction steps, respectively
+    assert output_1 == (set([8,1,5]), set([11,6,2]))
+    assert output_2 == (set([8,1,5,4]), set([11,6,2,9]))
+
+    assert set([G.node[n]['dist'] for n in [8,11]]) == set([0])
+    assert set([G.node[n]['dist'] for n in [1,2,5,6,12]]) == set([1])
+    assert set([G.node[n]['dist'] for n in [3,4,7,9]]) == set([2])
+
+    # Test for detection of non-reachable nodes
+    # Only nodes 1, 2, 3, 4, 5 and 6 should be reachable and will receive a 'dist' value - other nodes do not
+    Y = nx.DiGraph()
+    Y.add_node(1,type='c',start=True)
+    Y.add_node(2,type='rf',c={1})
+    Y.add_node(3,type='pf',c={6})
+    Y.add_node(6,type='c',start=False)
+    Y.add_path([1,2,3,6])
+    Y.add_node(4,type='rr',c={6})
+    Y.add_node(5,type='pr',c={1})
+    Y.add_path([6,4,5,1])
+
+    Y.add_node(7,type='c',start=False)
+    Y.add_node(8,type='rf',c={6,7})
+    Y.add_node(9,type='pf',c={12})
+    Y.add_node(12,type='c',start=False)
+    Y.add_path([7,8,9,12])
+    Y.add_edge(6,8)
+    Y.add_node(10,type='rr',c={12})
+    Y.add_node(11,type='pr',c={6,7})
+    Y.add_path([6,8,9,12])
+    Y.add_edge(12,10)
+
+    output_Y = DistanceToOrigin(Y, -1)
+
+    z = 0
+    for node in Y.nodes():
+        try:
+            x = Y.node[node]['dist']
+        except KeyError:
+            z += 1
+
+    assert z == 6
+    assert [Y.node[n]['dist'] for n in range(1,7)] == [0,0,1,1,2,1]
+
+
+def PruneNetwork(network):
+    """"""
+
+    return None
+
+def test_PruneNetwork():
+    # Nodes that were not reached in DistanceToOrigin are going to lack the 'dist' data key
+    # Such nodes are expected to be removed
+    G = nx.DiGraph
+    assert PruneNetwork()
 
 
 def DetectLoops(path):
